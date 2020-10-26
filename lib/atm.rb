46f3ad866083bf4d809311b2788ada084f1a02fe
William <willspecker@gmail.com>
2020-10-26 17:31:06 +0100

    Skapar en instance double för att skriva nya prov, modifierar withdraw metoden i implementation code, refaktor så att det finns bara en metod till varje uppgift, skriver ett sad path prov

--- lib/atm.rb
+++ lib/atm.rb
@@ -3,7 +3,27 @@ class Atm
     def initialize
         @funds = 1000
     end
-    def withdraw(amount)
+
+    def withdraw(amount, account)
+        case 
+        when insufficient_funds_in_account?(amount, account)
+            { status: false, message: 'insufficient funds', date: Date.today }
+        else
+            perform_transaction(amount, account)
+        end
+        
+    end
+
+    private
+
+    def insufficient_funds_in_account?(amount, account)
+        amount > account.balance
+    end
+
+    def perform_transaction(amount, account)
         @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount: amount }
     end
 end 
+
