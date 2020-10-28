9d7dff3f5a98ed4971d29a308e476deefbd86335
viktor <viktorkarlsson125@gmail.com>
2020-10-28 17:59:09 +0100

    integrating all test. no addition to application.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -36,16 +36,39 @@ describe Person do
     descirbe 'can manage funds if an account been created' do
         let(:atm) { Atm.new }
         before { subject.create_account }
-            it 'can deposit funds' do 
-                expect(subject.deposit(100)).to be_truthy
-            end
-            descibe 'can not manage funds if no account been created' do #bör kanske flyttas
-                it 'can not deposit funds' do
-                    expect { subject.deposit(100) }.to raise_error(RuntimeError, 'no account pressent')
-                end 
-            end
-            
-    end
+        it 'can deposit funds' do 
+            expect(subject.deposit(100)).to be_truthy
+        end
 
-    
+        descibe 'can not manage funds if no account been created' do #bör kanske flyttas
+            it 'can not deposit funds' do
+                expect { subject.deposit(100) }.to raise_error(RuntimeError, 'no account pressent')
+            end 
+        end
+
+        it 'funds are added to the account balance - deducted from cash' do
+          subject.cash = 100
+          subject.deposit(100)
+          expect(subject.account.balance).to be 100
+          expect(subject.cash).to be 0
+        end
+
+        it 'can withdraw funds' do
+            command = lambda {subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm) }
+            expect(command.call).to be_truthy
+        end
+
+        it 'withdraw is expected to raise an error if no ATM is passed in' do
+            command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account}
+            expect { command.call }.to raise_eroor 'An ATM is required'
+        end
+
+        it 'funds are added to cash - deducted from account balance' do
+            subject.cash = 100
+            subject.deposit(100)
+            subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm)
+            expect(subject.account.balance).to be 0
+            expect(subject.cash).to be 100
+        end
+    end    
 end
